5936ae42fde54ff4923278dd9e77060c2de95618
Ala' Alnajjar <alaa9jo@yahoo.com>
2016-07-12 21:41:06 +0300

    Update product.py

--- models/product.py
+++ models/product.py
@@ -6,7 +6,7 @@ import base64
 class Product(models.Model):
     _inherit = 'product.template'
 
-    qty_low_stock_notify = fields.Integer(string='Notify for Qty Below', default=80,
+    qty_low_stock_notify = fields.Integer(string='Notify for Qty Below', default=-1,
                                           help='When stock on hand falls below this number, it will be included in the low stock report. Set to -1 to exclude from the report.')
 
     def send_low_stock_via_email(self, cr, uid, context=None):
@@ -27,7 +27,7 @@ class Product(models.Model):
             """ %(header_label_list[0], header_label_list[1], header_label_list[2], header_label_list[3], header_label_list[4])
             ## Check for low stock products
             product_obj  = self.pool.get('product.product')
-            product_ids  = product_obj.search(cr, uid, [('active', '=', True), ('sale_ok', '=', True), ('default_code', '!=', False)])
+            product_ids  = product_obj.search(cr, uid, [('active', '=', True), ('default_code', '!=', False)])
             for product in product_obj.browse(cr, uid, product_ids):
                 product_sku = product.default_code
                 if not product_sku or product_sku == '':
